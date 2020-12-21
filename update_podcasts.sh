31f09e5c98670c7144c33d47be87c5e84e839821
Sebastian Mangelsen <sebastian.mangelsen@semcon.com>
2020-12-21 02:21:39 +0100

    fix user permissions

--- update_podcasts.sh
+++ update_podcasts.sh
@@ -2,12 +2,22 @@
 
 IMAGE_NAME="smangelsen/poca:latest"
 THREADS=$(nproc)
-CMD="poca -c /usr/share/config/poca -t ${THREADS}"
+CMD="poca -c /poca_config -t ${THREADS}"
+
+if ! test -d $(pwd)/podcasts; then
+	mkdir -p $(pwd)/podcasts
+fi
+
+if ! test -d $(pwd)/poca_config; then
+	mkdir -p $(pwd)/poca_config
+	cp poca.xml	$(pwd)/poca_config/.
+fi
 
 docker run -t -i \
 	--rm \
+	-u $(id -u):$(id -g) \
 	-v $(pwd)/podcasts:/podcasts \
-	-v $(pwd)/poca_config:/usr/share/config/poca \
+	-v $(pwd)/poca_config:/poca_config \
 	"${IMAGE_NAME}" \
 	$CMD
 
