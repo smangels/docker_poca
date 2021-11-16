da093c297b6a0d80691cece6b76f887314f02335
Sebastian Mangelsen (Github) <github.mangelsen.se>
2021-11-17 00:08:50 +0100

    improve SHELL script

--- update_podcasts.sh
+++ update_podcasts.sh
@@ -1,23 +1,34 @@
 #!/usr/bin/env bash
 
+PWD=$(pwd)
+
+function _cleanup {
+	rm -rf ${PWD}/poca_config/
+}
+
+trap _cleanup EXIT
+
+
 IMAGE_NAME="smangelsen/poca:latest"
 THREADS=$(nproc)
 CMD="poca -c /poca_config -t ${THREADS}"
 
-if ! test -d $(pwd)/podcasts; then
-	mkdir -p $(pwd)/podcasts
+if ! test -d ${PWD}/podcasts; then
+	mkdir -p ${PWD}/podcasts
 fi
 
-if ! test -d $(pwd)/poca_config; then
-	mkdir -p $(pwd)/poca_config
-	cp poca.xml	$(pwd)/poca_config/.
+if ! test -d ${PWD}/poca_config; then
+	mkdir -p ${PWD}/poca_config
+else
+	rm -rf ${PWD}/poca_config/*.xml
 fi
+
+cp poca.xml	${PWD}/poca_config/.
 
 docker run -t -i \
 	--rm \
 	-u $(id -u):$(id -g) \
-	-v $(pwd)/podcasts:/podcasts \
-	-v $(pwd)/poca_config:/poca_config \
+	-v ${PWD}/podcasts:/podcasts \
+	-v ${PWD}/poca_config:/poca_config \
 	"${IMAGE_NAME}" \
 	$CMD
-
